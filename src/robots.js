c21df0cb8e900a12ea39966691f7febd0d66246d
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-04-27 15:34:17 +0530

    added Card component to site

---
+++ src/robots.js
@@ -0,0 +1,14 @@
+export const robots=[
+    {
+        id: 1,
+        name:'Dasith',
+        username: 'masterD',
+        email: 'dasithrathnasinghe@gmail.com'
+    },
+    {
+        id: 2,
+        name:'hasith',
+        username: 'mr.H',
+        email: 'hasithrathnasinghe@gmail.com'
+    }
+]
