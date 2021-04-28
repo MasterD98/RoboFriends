77a7e1f7087cd7f714fc7b4142fab688488316d9
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-04-28 22:49:49 +0530

    used Rest API https request instend of robots.js to get User details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import CardList from './CardList'
-import {robots} from './robots'
 import SearchBox from './SearchBox'
 
 class App extends React.Component {
     constructor(){
         super();
         this.state={
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
 
@@ -15,19 +14,27 @@ class App extends React.Component {
     onSearchChange = (event) => {
         this.setState({searchfield : event.target.value})
     }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users=>{this.setState({robots:users})});
+    }
 
     render(){
         const filteredRobots=this.state.robots.filter(robots=>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         } )
-        return(
-            <div className= 'tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox onSearchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        )
-        
+        if(this.state.robots.length===0){
+            return <h1>Loading..</h1>
+        }else{
+            return(
+                <div className= 'tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox onSearchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 } 
 export default App; 
