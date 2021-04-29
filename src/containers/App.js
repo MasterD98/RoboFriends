5aa1ed00674c796df053578b7124337b06669cce
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-04-29 13:50:30 +0530

    Refactor code and restrucure folder base

---
+++ src/containers/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            robots: [],
+            searchfield: ''
+        }
+
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield : event.target.value})
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users=>{this.setState({robots:users})});
+    }
+
+    render(){
+        const {robots,searchfield}=this.state;
+        const filteredRobots=robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        } )
+        return !robots.length ?
+            <h1>Loading..</h1>:
+            (
+            <div className= 'tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox onSearchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+            )
+    }
+} 
+export default App; 
