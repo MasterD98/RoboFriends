4ba67056f07962c62dc948f4f21b0635bc4eb829
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-05-03 19:00:29 +0530

    Used API.js to fetch user informations

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,7 @@ import {CHANGE_SEARCH_FIELD,
         REQUEST_ROBOTS_SUCCESS,
         REQUEST_ROBOTS_FAILED
 } from './constants.js';
+import {apiCall} from './API/api.js';
 
 export const setSearchField=(text)=>({
     type : CHANGE_SEARCH_FIELD,
@@ -10,9 +11,8 @@ export const setSearchField=(text)=>({
 })
 
 export const requestRobots=(dispatch)=>{
-    dispatch({type : REQUEST_ROBOTS_PENDING});
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(data=>dispatch({type: REQUEST_ROBOTS_SUCCESS,payload : data}))
-        .catch(error=>dispatch({type: REQUEST_ROBOTS_FAILED,payload : error}));
+    dispatch({ type: REQUEST_ROBOTS_PENDING })
+  apiCall('https://jsonplaceholder.typicode.com/users')
+    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
 }
